6db07332a5d701308e0be560e95cbf4e56273291
Brad Quinn <bradleyquinn@live.com>
2016-05-28 08:46:12 -0700

    MasterPage UI polish      - Update CSS classes to correct size and orientation      - Remove Contact label      - Change link text color

--- BWQ.TripCalculator/Main.Master.designer.cs
+++ BWQ.TripCalculator/Main.Master.designer.cs
@@ -67,15 +67,6 @@ namespace BWQ.TripCalculator {
         protected global::System.Web.UI.WebControls.Panel pnlBotNav;
         
         /// <summary>
-        /// lblContact control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblContact;
-        
-        /// <summary>
         /// lblName control.
         /// </summary>
         /// <remarks>
